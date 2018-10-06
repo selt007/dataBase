c7d2593b78017f2f7fb498e133a4257ccc8a183f
Alexandr <all1992ex@gmail.com>
2018-10-06 12:02:32 +0400

    com

---
+++ DataBaseUsers/MainForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DataBaseUsers
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonAddID = new System.Windows.Forms.Button();
+            this.panelData = new System.Windows.Forms.Panel();
+            this.textBoxAddID = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUpdate.BackColor = System.Drawing.Color.OldLace;
+            this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUpdate.Location = new System.Drawing.Point(266, 12);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(101, 54);
+            this.buttonUpdate.TabIndex = 0;
+            this.buttonUpdate.Text = "Обновить файлы";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxSearch.Location = new System.Drawing.Point(12, 12);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(248, 21);
+            this.textBoxSearch.TabIndex = 1;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // buttonAddID
+            // 
+            this.buttonAddID.BackColor = System.Drawing.Color.Gainsboro;
+            this.buttonAddID.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAddID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAddID.Location = new System.Drawing.Point(155, 39);
+            this.buttonAddID.Name = "buttonAddID";
+            this.buttonAddID.Size = new System.Drawing.Size(105, 27);
+            this.buttonAddID.TabIndex = 3;
+            this.buttonAddID.Text = "Добавить ID";
+            this.buttonAddID.UseVisualStyleBackColor = false;
+            this.buttonAddID.Click += new System.EventHandler(this.buttonAddID_Click);
+            // 
+            // panelData
+            // 
+            this.panelData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelData.Location = new System.Drawing.Point(12, 72);
+            this.panelData.Name = "panelData";
+            this.panelData.Size = new System.Drawing.Size(355, 269);
+            this.panelData.TabIndex = 4;
+            // 
+            // textBoxAddID
+            // 
+            this.textBoxAddID.Location = new System.Drawing.Point(12, 43);
+            this.textBoxAddID.Mask = "ID-00000";
+            this.textBoxAddID.Name = "textBoxAddID";
+            this.textBoxAddID.PromptChar = 'X';
+            this.textBoxAddID.Size = new System.Drawing.Size(137, 20);
+            this.textBoxAddID.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(379, 353);
+            this.Controls.Add(this.textBoxAddID);
+            this.Controls.Add(this.panelData);
+            this.Controls.Add(this.buttonAddID);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonUpdate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "База данных";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonAddID;
+        private System.Windows.Forms.Panel panelData;
+        private System.Windows.Forms.MaskedTextBox textBoxAddID;
+    }
+}
+
---
+++ DataBaseUsers/MainForm.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace DataBaseUsers
+{
+    public partial class MainForm : Form
+    {
+        static string pathUsers = "users";
+        static int countUsers = Directory.GetFiles(pathUsers).Length - 2;
+
+        Panel[] rows = new Panel[countUsers];
+        DateTimePicker[] col3 = new DateTimePicker[countUsers];
+        Label[] col2 = new Label[countUsers];
+        CheckBox[] col1 = new CheckBox[countUsers];
+        string[] arrNameFile = Directory.GetFiles(pathUsers);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxSearch.SetWatermark("Начните вводить ID для поиска...");
+
+            if (!File.Exists("users.xml"))
+            {
+                File.Create("users.xml").Close();
+                File.WriteAllText("users.xml", "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<users>\n</users>");
+            }
+
+            LoadUsers();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            string str2M3U = File.ReadAllText($"{pathUsers}//2.M3U");
+            string str1M3U = File.ReadAllText($"{pathUsers}//1.M3U");
+
+            XDocument xdoc = XDocument.Load("users.xml");
+            XElement root = xdoc.Element("users");
+
+            for (int i = 2; i < rows.Length; i++)
+            {
+                if (!col1[i].Checked)
+                {
+                    //File.WriteAllText(arrNameFile[i], "");
+                    //File.WriteAllText(arrNameFile[i], str2M3U);
+                    foreach (XElement xe in root.Elements("user").ToList())
+                    {
+                        if (xe.Element("id").Value == "users\\" + arrNameFile[i].Replace(".M3U", ""))
+                        {
+                            xe.Element("chk").Value = "true";
+                            //xe.Element("date").Value = col3[i - 2].Text;
+                        }
+                    }
+                }
+                else
+                {
+                    //File.WriteAllText(arrNameFile[i], "");
+                    //File.WriteAllText(arrNameFile[i], str1M3U);
+                    foreach (XElement xe in root.Elements("user").ToList())
+                    {
+                        if (xe.Element("id").Value == "users\\" + arrNameFile[i].Replace(".M3U", ""))
+                        {
+                            xe.Element("chk").Value = "false";
+                            //xe.Element("date").Value = col3[i - 2].Text;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonAddID_Click(object sender, EventArgs e)
+        {
+            AddInXml(textBoxAddID.Text);
+            LoadUsers();
+        }
+
+        void LoadUsers()
+        {
+            bool chk = false;
+            string date = string.Empty, ID = string.Empty;
+            int i = 0;
+            
+            XDocument xdoc = XDocument.Load("users.xml");
+            try
+            {
+                foreach (XElement user in xdoc.Element("users").Elements("user"))
+                {
+                    XElement xChk = user.Element("chk");
+                    XElement xID = user.Element("id");
+                    XElement xDate = user.Element("date");
+                        
+                    if (xChk.Value == "true")
+                        chk = true;
+                    else chk = false;
+                    ID = xID.Value;
+                    date = xDate.Value;
+
+                    rows[i] = new Panel();
+                    rows[i].Location = new System.Drawing.Point(0, i * 22);
+                    rows[i].Name = "row" + i.ToString();
+                    rows[i].Size = new System.Drawing.Size(panelData.Width, 22);
+                    rows[i].BorderStyle = BorderStyle.FixedSingle;
+
+                    col3[i] = new DateTimePicker();
+                    col3[i].Location = new System.Drawing.Point(panelData.Width - 131, 0);
+                    col3[i].Name = "3column" + i.ToString();
+                    col3[i].Size = new System.Drawing.Size(130, 28);
+                    col3[i].Text = date;
+
+                    col2[i] = new Label();
+                    col2[i].Location = new System.Drawing.Point(30, 3);
+                    col2[i].Name = "2column" + i.ToString();
+                    col2[i].Text = ID;
+                    col2[i].Size = new System.Drawing.Size(panelData.Width - 167, 22);
+
+                    col1[i] = new CheckBox();
+                    col1[i].Location = new System.Drawing.Point(2, 0);
+                    col1[i].Name = "1column" + i.ToString();
+                    col1[i].Size = new System.Drawing.Size(35, 20);
+                    col1[i].Checked = chk;
+
+                    rows[i].Controls.Add(col3[i]);
+                    rows[i].Controls.Add(col2[i]);
+                    rows[i].Controls.Add(col1[i]);
+                    panelData.Controls.Add(rows[i]);
+                    i++;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Необходим перезапуск программы для создания файлов!\n" +
+                    "Вероятно, записи в файле конфигурации и файлы в папке с пользователями разнятся." +
+                    "\nДополнительная информация:\n" + ex,
+                                    "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        void AddInXml(string name)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            XmlElement user = xDoc.CreateElement("user");
+
+            XmlElement _chk = xDoc.CreateElement("chk");
+            XmlElement _id = xDoc.CreateElement("id");
+            XmlElement _date = xDoc.CreateElement("date");
+
+            XmlText chk = xDoc.CreateTextNode("false");
+            XmlText id = xDoc.CreateTextNode(name);
+            XmlText date = xDoc.CreateTextNode(DateTime.Now.Date.ToString());
+
+            if (name == "ID-")
+            {
+                MessageBox.Show("Нельзя оставлять поля пустыми!",
+                    "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                foreach (XmlNode xnode in xRoot)
+                {
+                    foreach (XmlNode childnode in xnode.ChildNodes)
+                    {
+                        if (childnode.Name == "email")
+                        {
+                            if (childnode.InnerText == name)
+                            {
+                                MessageBox.Show("Введеный пользователь уже добавлен!",
+                                    "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                        }
+                    }
+                }
+                _chk.AppendChild(chk);
+                _id.AppendChild(id);
+                _date.AppendChild(date);
+
+                user.AppendChild(_chk);
+                user.AppendChild(_id);
+                user.AppendChild(_date);
+
+                xRoot.AppendChild(user);
+                xDoc.Save("users.xml");
+            }
+            CreateNewFile(textBoxAddID.Text);
+        }
+
+        void CreateNewFile(string name)
+        {
+            if (!File.Exists($"{pathUsers}//{name}.M3U"))
+            {
+                File.Create($"{pathUsers}//{name}.M3U").Close();
+                string str2M3U = File.ReadAllText($"{pathUsers}//2.M3U");
+                File.WriteAllText($"{pathUsers}//{name}.M3U", str2M3U);
+            }
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
